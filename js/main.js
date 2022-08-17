7a6f2b407bd445c97048d0531bbc45ef5f42a459
EmanuelKrivich <emanuel.krivich@gmail.com>
2022-08-17 15:06:55 -0300

    incorpore eventos

--- js/main.js
+++ js/main.js
@@ -1,118 +1,107 @@
-// CLASE Y CONSTUCTOR DE OBJETOS
-class Discos {
-    constructor(artista, genero, disco, precio) {
-        this.artista = artista;
-        this.genero = genero;
-        this.disco = disco;
-        this.precio = precio;
+let btnRock = document.getElementById('btnRock');
+let btnJazz = document.getElementById('btnJazz');
+let btnBlues = document.getElementById('btnBlues');
+let btnSoul = document.getElementById('btnSoul');
+let btnDisco = document.getElementById('btnDisco');
+let btnClasico = document.getElementById('btnClasico');
+let btnBuscar = document.getElementById('btnBuscar');
+let inputBuscar = document.getElementById('inputBuscar');
+
+btnRock.onclick = () => {
+
+    document.querySelector("#imgRock").style.display = "none";
+    document.querySelector("#btnRock").style.display = "none";
+
+    let h5 = document.createElement("h5");
+    h5.innerHTML = "Rock";
+    document.body.appendChild(h5);
+
+    for (index = 0; index < rock.length; index++) {
+        let ulRock = document.createElement("ul");
+        ulRock.innerHTML = `<li>${rock[index].artista} - ${rock[index].disco}</li>`;
+        document.body.appendChild(ulRock);
     }
 }
 
-// // CREACION DE ARRAYS
-const rock = [];
-const soul = [];
-const jazz = [];
+btnJazz.onclick = () => {
 
-// CREACION DE OBJETOS Y METODO PUSH PARA INCORPORAR OBJETOS A LOS ARRAY
-rock.push(new Discos("Led Zeppelin", "Rock", "Houses of the Holy", 4200));
-rock.push(new Discos("Led Zeppelin", "Rock", "Physical Graffiti", 4000));
-rock.push(new Discos("Pink Floyd", "Rock", "The Dark Side of the Moon", 5000));
-rock.push(new Discos("Deep Purple", "Rock", "Machine Head", 3700));
-rock.push(new Discos("Van Halen", "Rock", "1984", 3500));
+    document.querySelector("#imgJazz").style.display = "none";
+    document.querySelector("#btnJazz").style.display = "none";
 
+    let h5 = document.createElement("h5");
+    h5.innerHTML = "Jazz";
+    document.body.appendChild(h5);
 
-soul.push(new Discos("Stevie Wonder", "Soul", "Innervisions", 4000));
-soul.push(new Discos("Lauryn Hill", "Soul", "The Miseducation of Lauryn Hill", 3200));
-soul.push(new Discos("Marvin gaye", "Soul", "Lets get it on", 3500));
-soul.push(new Discos("Sam Cooke", "Soul", "Mr. Soul", 4000))
-
-jazz.push(new Discos("Miles Davis", "Jazz", "Kind of Blue", 4100));
-jazz.push(new Discos("Jamie Cullum", "Jazz", "Twentysomething", 3200));
-jazz.push(new Discos("Louis Armstrong", "Jazz", "What a Wonderful World", 4000));
-jazz.push(new Discos("Thelonious Monk", "Jazz", "The Unique Thelonious Monk", 3800));
-
-
-// FUNCION PARA REALIZAR DESCUENTO AL PRECIO
-function descuento() {
-    let precioDescontado = (valor - ((valor * 25) / 100));
-    alert("Con el descuento usted abonará: $" + precioDescontado);
-}
-
-
-
-// SELECCION DE GENERO MUSICAL
-let bienvenida = prompt("¡Bienvendo a DeVinilo! Elija un género musical entre Rock, Soul o Jazz, o presione s para recibir una oferta limitada. Presione n para salir.");
-
-while ((bienvenida != "s") && (bienvenida != "n")) {
-    if ((bienvenida == "Rock") || (bienvenida == "rock")) {
-        let filtroGenero = rock.filter((elemento) => elemento.genero == "Rock");
-        console.log(filtroGenero);
-        break;
-
-    } else if ((bienvenida == "Soul") || (bienvenida == "soul")) {
-        let filtroGenero = soul.filter((elemento) => elemento.genero == "Soul");
-        console.log(filtroGenero);
-        break;
-
-    } else if ((bienvenida == "Jazz") || (bienvenida == "jazz")) {
-        let filtroGenero = jazz.filter((elemento) => elemento.genero == "Jazz");
-        console.log(filtroGenero);
-        break;
-        
-    } else {
-        alert("Ingrese un género válido");
-        bienvenida = prompt("Vuelva a ingresar un género entre Rock, Soul o Jazz, o presione s para recibir una oferta limitada. Presione n para salir.")
+    for (index = 0; index < jazz.length; index++) {
+        let ulJazz = document.createElement("ul");
+        ulJazz.innerHTML = `<li>${jazz[index].artista} - ${jazz[index].disco}</li>`;
+        document.body.appendChild(ulJazz);
     }
 }
 
+btnSoul.onclick = () => {
 
-// SELECCION DE DISCO EN OFERTA
-while (bienvenida == "s") {
-    let discoSeleccionado = parseInt(prompt("Puede adquirir uno de estos discos con un 25% de descuento! Seleccione presionando 1,2,3 o 4: " + "\n1-" + rock[0].artista + "-" + rock[0].disco + "\n2-" + rock[1].artista + "-" + rock[1].disco + "\n3-" + rock[2].artista + "-" + rock[2].disco + "\n4-" + soul[0].artista + "-" + soul[0].disco));
-    switch (discoSeleccionado) {
-        case 1:
-            alert("Usted seleccionó " + rock[0].artista + "-" + rock[0].disco + ": $" + rock[0].precio);
-            valor = rock[0].precio;
-            descuento();
-            break;
-        case 2:
-            alert("Usted seleccionó " + rock[1].artista + "-" + rock[1].disco + ": $" + rock[1].precio);
-            valor = rock[1].precio;
-            descuento();
-            break;
-        case 3:
-            alert("Usted seleccionó " + rock[2].artista + "-" + rock[2].disco + ": $" + rock[2].precio);
-            valor = rock[2].precio;
-            descuento();
-            break;
-        case 4:
-            alert("Usted seleccionó " + soul[0].artista + "-" + soul[0].disco + ": $" + soul[0].precio);
-            valor = soul[0].precio;
-            descuento();
-            break;
-        default:
-            alert("Ingrese un número válido");
+    document.querySelector("#imgSoul").style.display = "none";
+    document.querySelector("#btnSoul").style.display = "none";
 
+    let h5 = document.createElement("h5");
+    h5.innerHTML = "Soul";
+    document.body.appendChild(h5);
+
+    for (index = 0; index < soul.length; index++) {
+        let ulSoul = document.createElement("ul");
+        ulSoul.innerHTML = `<li>${soul[index].artista} - ${soul[index].disco}</li>`;
+        document.body.appendChild(ulSoul);
     }
-    bienvenida = prompt("Si desea seleccionar otro disco presione s, si desea salir presione n");
-
-
 }
 
-function mostrarDiscosDeRock() {
-    let filtroGenero = rock.filter((elemento) => elemento.genero == "Rock");
-    console.log(filtroGenero);
+btnDisco.onclick = () => {
 
+    document.querySelector("#imgDisco").style.display = "none";
+    document.querySelector("#btnDisco").style.display = "none";
 
-    listadoLI = document.getElementById('listadoRock');
+    let h5 = document.createElement("h5");
+    h5.innerHTML = "Disco";
+    document.body.appendChild(h5);
 
-    document.querySelector('.generos').style.display = "none";
+    for (index = 0; index < disco.length; index++) {
+        let ulDisco = document.createElement("ul");
+        ulDisco.innerHTML = `<li>${disco[index].artista} - ${disco[index].disco}</li>`;
+        document.body.appendChild(ulDisco);
+    }
+}
 
+btnBlues.onclick = () => {
+
+    document.querySelector("#imgBlues").style.display = "none";
+    document.querySelector("#btnBlues").style.display = "none";
+
+    let h5 = document.createElement("h5");
+    h5.innerHTML = "Blues";
+    document.body.appendChild(h5);
+
+    for (index = 0; index < blues.length; index++) {
+        let ulBlues = document.createElement("ul");
+        ulBlues.innerHTML = `<li>${blues[index].artista} - ${blues[index].disco}</li>`;
+        document.body.appendChild(ulBlues);
+    }
+}
+
+btnClasico.onclick = () => {
+
+    document.querySelector("#imgClasico").style.display = "none";
+    document.querySelector("#btnClasico").style.display = "none";
     
+    let h5 = document.createElement("h5");
+    h5.innerHTML = "Clasico";
+    document.body.appendChild(h5);
+
+    for (index = 0; index < clasico.length; index++) {
+        let ulClasico = document.createElement("ul");
+        ulClasico.innerHTML = `<li>${clasico[index].artista} - ${clasico[index].disco}</li>`;
+        document.body.appendChild(ulClasico);
+    }
 }
 
-retornarNodoLI() {
-    let nodo = document.createElement('li');
-    nodo.innerText = `${this.nombre} -- ${this.ayuda}`;
-    return nodo;
-}
+
+
