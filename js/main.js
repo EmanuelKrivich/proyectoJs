eae386f82b64f6dfb5a026ff8e75318c0b178f77
EmanuelKrivich <emanuel.krivich@gmail.com>
2022-08-17 17:31:08 -0300

    reduciendo codigo con funcion

--- js/main.js
+++ js/main.js
@@ -1,108 +1,77 @@
+// CLASE Y CONSTUCTOR DE OBJETOS
+class Discos {
+    constructor(artista, genero, disco, precio) {
+        this.artista = artista;
+        this.genero = genero;
+        this.disco = disco;
+        this.precio = precio;
+    }
+}
+
+// // CREACION DE ARRAYS
+const rock = [];
+const soul = [];
+const jazz = [];
+const disco = [];
+const blues = [];
+const clasico = [];
+
+// CREACION DE OBJETOS Y METODO PUSH PARA INCORPORAR OBJETOS A LOS ARRAY
+rock.push(new Discos("Led Zeppelin", "Rock", "Houses of the Holy", 4200));
+rock.push(new Discos("Led Zeppelin", "Rock", "Physical Graffiti", 4000));
+rock.push(new Discos("Pink Floyd", "Rock", "The Dark Side of the Moon", 5000));
+rock.push(new Discos("Deep Purple", "Rock", "Machine Head", 3700));
+rock.push(new Discos("Van Halen", "Rock", "1984", 3500));
+soul.push(new Discos("Stevie Wonder", "Soul", "Innervisions", 4000));
+soul.push(new Discos("Lauryn Hill", "Soul", "The Miseducation of Lauryn Hill", 3200));
+soul.push(new Discos("Marvin gaye", "Soul", "Lets get it on", 3500));
+soul.push(new Discos("Sam Cooke", "Soul", "Mr. Soul", 4000))
+jazz.push(new Discos("Miles Davis", "Jazz", "Kind of Blue", 4100));
+jazz.push(new Discos("Jamie Cullum", "Jazz", "Twentysomething", 3200));
+jazz.push(new Discos("Louis Armstrong", "Jazz", "What a Wonderful World", 4000));
+jazz.push(new Discos("Thelonious Monk", "Jazz", "The Unique Thelonious Monk", 3800));
+disco.push(new Discos("Kool and the Gang", "Disco", "Celebrate!", 4200));
+disco.push(new Discos("Bee Gees", "Disco", "Saturday Night Fever", 4500));
+disco.push(new Discos("Chic", "Disco", "Cest Chic", 3800));
+blues.push(new Discos("B.B. King", "Blues", "Indianola Mississippi Seeds", 4200));
+blues.push(new Discos("Joe Cocker", "Blues", "With a Little Help from My Friends", 4500));
+blues.push(new Discos("Muddy Waters", "Blues", "The Muddy Waters Woodstock Album", 3800));
+clasico.push(new Discos("Jordi Savall", "Clasico", "The Celtic Viol", 4200));
+clasico.push(new Discos("Christina Pluhar", "Clasico", "La Tarantella: Antidotum Tarantulae", 4500));
+clasico.push(new Discos("Juan Diego Flórez", "Clasico", "Mozart", 3800));
+
+
 let btnRock = document.getElementById('btnRock');
 let btnJazz = document.getElementById('btnJazz');
 let btnBlues = document.getElementById('btnBlues');
 let btnSoul = document.getElementById('btnSoul');
 let btnDisco = document.getElementById('btnDisco');
 let btnClasico = document.getElementById('btnClasico');
-let btnBuscar = document.getElementById('btnBuscar');
-let inputBuscar = document.getElementById('inputBuscar');
 
 
-btnRock.onclick = () => {
+const mostrarLista = (btn, idBtn, idImg, tituloH5, array) => {
+    btn.onclick = () => {
+        document.querySelector(idBtn).style.display = "none";
+        document.querySelector(idImg).style.display = "none";
 
-    document.querySelector("#imgRock").style.display = "none";
-    document.querySelector("#btnRock").style.display = "none";
+        let h5 = document.createElement("h5");
+        h5.innerHTML = tituloH5;
+        document.body.appendChild(h5);
 
-    let h5 = document.createElement("h5");
-    h5.innerHTML = "Rock";
-    document.body.appendChild(h5);
-
-    for (index = 0; index < rock.length; index++) {
-        let ulRock = document.createElement("ul");
-        ulRock.innerHTML = `<li>${rock[index].artista} - ${rock[index].disco}</li>`;
-        document.body.appendChild(ulRock);
+        for (index = 0; index < rock.length; index++) {
+            let ul = document.createElement("ul");
+            ul.innerHTML = `<li>${array[index].artista} - ${array[index].disco}</li>`;
+            document.body.appendChild(ul);
+        }
     }
 }
 
-btnJazz.onclick = () => {
-
-    document.querySelector("#imgJazz").style.display = "none";
-    document.querySelector("#btnJazz").style.display = "none";
-
-    let h5 = document.createElement("h5");
-    h5.innerHTML = "Jazz";
-    document.body.appendChild(h5);
-
-    for (index = 0; index < jazz.length; index++) {
-        let ulJazz = document.createElement("ul");
-        ulJazz.innerHTML = `<li>${jazz[index].artista} - ${jazz[index].disco}</li>`;
-        document.body.appendChild(ulJazz);
-    }
-}
-
-btnSoul.onclick = () => {
-
-    document.querySelector("#imgSoul").style.display = "none";
-    document.querySelector("#btnSoul").style.display = "none";
-
-    let h5 = document.createElement("h5");
-    h5.innerHTML = "Soul";
-    document.body.appendChild(h5);
-
-    for (index = 0; index < soul.length; index++) {
-        let ulSoul = document.createElement("ul");
-        ulSoul.innerHTML = `<li>${soul[index].artista} - ${soul[index].disco}</li>`;
-        document.body.appendChild(ulSoul);
-    }
-}
-
-btnDisco.onclick = () => {
-
-    document.querySelector("#imgDisco").style.display = "none";
-    document.querySelector("#btnDisco").style.display = "none";
-
-    let h5 = document.createElement("h5");
-    h5.innerHTML = "Disco";
-    document.body.appendChild(h5);
-
-    for (index = 0; index < disco.length; index++) {
-        let ulDisco = document.createElement("ul");
-        ulDisco.innerHTML = `<li>${disco[index].artista} - ${disco[index].disco}</li>`;
-        document.body.appendChild(ulDisco);
-    }
-}
-
-btnBlues.onclick = () => {
-
-    document.querySelector("#imgBlues").style.display = "none";
-    document.querySelector("#btnBlues").style.display = "none";
-
-    let h5 = document.createElement("h5");
-    h5.innerHTML = "Blues";
-    document.body.appendChild(h5);
-
-    for (index = 0; index < blues.length; index++) {
-        let ulBlues = document.createElement("ul");
-        ulBlues.innerHTML = `<li>${blues[index].artista} - ${blues[index].disco}</li>`;
-        document.body.appendChild(ulBlues);
-    }
-}
-
-btnClasico.onclick = () => {
-
-    document.querySelector("#imgClasico").style.display = "none";
-    document.querySelector("#btnClasico").style.display = "none";
-    
-    let h5 = document.createElement("h5");
-    h5.innerHTML = "Clasico";
-    document.body.appendChild(h5);
-
-    for (index = 0; index < clasico.length; index++) {
-        let ulClasico = document.createElement("ul");
-        ulClasico.innerHTML = `<li>${clasico[index].artista} - ${clasico[index].disco}</li>`;
-        document.body.appendChild(ulClasico);
-    }
-}
+mostrarLista (btnRock, "#btnRock", "#imgRock", "Rock", rock);
+mostrarLista (btnSoul, "#btnSoul", "#imgSoul", "Soul", soul);
+mostrarLista (btnJazz, "#btnJazz", "#imgJazz", "Jazz", jazz);
+mostrarLista (btnDisco, "#btnDisco", "#imgDisco", "Disco", disco);
+mostrarLista (btnBlues, "#btnBlues", "#imgBlues", "Blues", blues);
+mostrarLista (btnClasico, "#btnClasico", "#imgClasico", "Clasico", clasico);
 
 
 
